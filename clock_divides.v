342d9c6fe1396e9ffc7240592d8708d2c05d3ab5
Vincent Lin <vinlin24@outlook.com>
2023-03-12 20:59:38 -0700

    Make pixel clock 16 Hz for normal 16 fps

--- clock_divides.v
+++ clock_divides.v
@@ -3,10 +3,10 @@ module clk_div(
     input wire clk,   // master clock: 50MHz
     input wire rst,   // asynchronous reset
     // Outputs
-    output wire dclk  // pixel clock: 25MHz
+    output wire dclk  // pixel clock: 16Hz (3.125M master clock pulses)
 );
 
-    reg [1:0] pixel_count;
+    integer pixel_count;
     reg pixel_clk_reg;
 
     initial begin
@@ -19,7 +19,7 @@ module clk_div(
             pixel_count <= 0;
             pixel_clk_reg <= 0;
         end
-        else if (pixel_count == 1) begin
+        else if (pixel_count == 3_125_000) begin
             pixel_count <= 0;
             pixel_clk_reg <= ~pixel_clk_reg;
         end
