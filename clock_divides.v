afd04c680452dc57d067fb411f1f64a3a61e2049
Vincent Lin <vinlin24@outlook.com>
2023-03-12 20:45:28 -0700

    Clean the formatting of *.v files

--- clock_divides.v
+++ clock_divides.v
@@ -1,37 +1,34 @@
 module clk_div(
-	// Inputs
-	input wire clk,						//master clock: 50MHz
-	input wire rst,						//asynchronous reset
-	// Outputs
-	output wire dclk//,						pixel clock: 25MHz
-	//output wire flying_saucer_clk,	//flying saucer clock: 1000Hz 
-	//output wire alien_clk				//alien clock: 100Hz
-	);
+    // Inputs
+    input wire clk,   // master clock: 50MHz
+    input wire rst,   // asynchronous reset
+    // Outputs
+    output wire dclk  // pixel clock: 25MHz
+);
 
-reg [1:0] pixel_count;
-reg pixel_clk_reg;
+    reg [1:0] pixel_count;
+    reg pixel_clk_reg;
 
-initial begin
-	pixel_count = 0;
-	pixel_clk_reg = 0;
-end
+    initial begin
+        pixel_count = 0;
+        pixel_clk_reg = 0;
+    end
 
-always @(posedge clk or posedge rst)
-begin
-	if (rst == 1) begin
-		pixel_count <= 0;
-		pixel_clk_reg <= 0;
-	end
-	else if (pixel_count == 1) begin
-		pixel_count <= 0;
-		pixel_clk_reg <= ~pixel_clk_reg;
-	end
-	else begin
-		pixel_count <= pixel_count + 1;
-		pixel_clk_reg <= pixel_clk_reg;
-	end
-end
- 
-assign dclk = pixel_clk_reg;
+    always @(posedge clk or posedge rst) begin
+        if (rst == 1) begin
+            pixel_count <= 0;
+            pixel_clk_reg <= 0;
+        end
+        else if (pixel_count == 1) begin
+            pixel_count <= 0;
+            pixel_clk_reg <= ~pixel_clk_reg;
+        end
+        else begin
+            pixel_count <= pixel_count + 1;
+            pixel_clk_reg <= pixel_clk_reg;
+        end
+    end
 
-endmodule 
+    assign dclk = pixel_clk_reg;
+
+endmodule
--- top.v
+++ top.v
@@ -1,44 +1,44 @@
 module top(
-	// Inputs
-	input wire clk,
-	// Buttons
-	input wire button_display,
-	// Outputs
-	output [7:0] rgb,
-	output wire hsync,
-	output wire vsync
+    // Inputs
+    input wire clk,
+    // Buttons
+    input wire button_display,
+    // Outputs
+    output [7:0] rgb,
+    output wire hsync,
+    output wire vsync
+);
+
+    // Wires for horizontal and vertical counters
+    wire [9:0] xCoord;
+    wire [9:0] yCoord;
+
+    // Wires for clocks
+    wire dclk;
+
+    // Generate display clock and in-game clock
+    clk_div clk_div(
+        .clk(clk),
+        .rst(button_display),
+        .dclk(dclk)
     );
 
-	// Wires for horizontal and vertical counters
-	wire [9:0] xCoord;
-	wire [9:0] yCoord;
-	
-	// Wires for clocks
-	wire dclk;
-	
-	// Generate display clock and in-game clock
-	clk_div clk_div(
-		.clk(clk),
-		.rst(button_display),
-		.dclk(dclk)
-	);
-	
-	// VGA controller
-	vga_controller controller(
-		.clk(dclk),
-		.rst(button_display),
-		.hsync(hsync),
-		.vsync(vsync),
-		.xCoord(xCoord),
-		.yCoord(yCoord)
-	);
-	
-	// VGA display
-	vga_display display(
-		.clk(clk),
-		.xCoord(xCoord),
-		.yCoord(yCoord),
-		.rgb(rgb)
-	);
+    // VGA controller
+    vga_controller controller(
+        .clk(dclk),
+        .rst(button_display),
+        .hsync(hsync),
+        .vsync(vsync),
+        .xCoord(xCoord),
+        .yCoord(yCoord)
+    );
+
+    // VGA display
+    vga_display display(
+        .clk(clk),
+        .xCoord(xCoord),
+        .yCoord(yCoord),
+        .rgb(rgb)
+    );
 
 endmodule
